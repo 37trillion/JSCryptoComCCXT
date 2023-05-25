64134bf1790dcd3e6330513a16700b36bbe8867e
37trillion <96641975+37trillion@users.noreply.github.com>
2023-05-25 01:10:04 +0000

    Update private.js

--- private.js
+++ private.js
@@ -1,16 +1,18 @@
 const ccxt = require('ccxt');
+const Web3 = require('web3');
+const BigNumber = require('bignumber.js');
 
 class PrivateAPI {
-  constructor() {
-    // Initialize the Crypto.com exchange object
+  constructor(apiKey, apiSecret) {
+    // Initialize Crypto.com exchange object
     this.exchange = new ccxt.cryptocom({
-      apiKey: 'YOUR_API_KEY',
-      secret: 'YOUR_API_SECRET',
-      // Add any additional required authentication parameters
+      apiKey,
+      secret: apiSecret,
     });
 
-    // Load markets
-    this.loadMarkets();
+    // Initialize Web3 provider
+    this.provider = new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID');
+    this.web3 = new Web3(this.provider);
   }
 
   async loadMarkets() {
@@ -18,10 +20,62 @@ class PrivateAPI {
       await this.exchange.loadMarkets();
     } catch (error) {
       console.error('Failed to load markets:', error);
+      throw error;
     }
   }
 
-  // Implement additional methods for interacting with the private API
+  async getTradablePairs() {
+    try {
+      const markets = await this.exchange.fetchMarkets();
+      return markets.map((market) => market.symbol);
+    } catch (error) {
+      console.error('Error retrieving tradable pairs:', error);
+      throw error;
+    }
+  }
+
+  async getAccountBalance() {
+    try {
+      const accountBalance = await this.exchange.fetchBalance();
+      return accountBalance;
+    } catch (error) {
+      console.error('Error retrieving account balance:', error);
+      throw error;
+    }
+  }
+
+  async getBlockNumber() {
+    try {
+      const blockNumber = await this.web3.eth.getBlockNumber();
+      return blockNumber;
+    } catch (error) {
+      console.error('Error retrieving block number:', error);
+      throw error;
+    }
+  }
+
+  async getBalance(address) {
+    try {
+      const balance = await this.web3.eth.getBalance(address);
+      return new BigNumber(balance);
+    } catch (error) {
+      console.error('Error retrieving balance:', error);
+      throw error;
+    }
+  }
+
+  async getGasPrice() {
+    try {
+      const gasPrice = await this.web3.eth.getGasPrice();
+      return new BigNumber(gasPrice);
+    } catch (error) {
+      console.error('Error retrieving gas price:', error);
+      throw error;
+    }
+  }
+
+  // Add additional private methods as needed
+
 }
 
 module.exports = {
